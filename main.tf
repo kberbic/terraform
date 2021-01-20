b3cad9560f5bcf8be1dc9e015ee4f3b1aadff8ce
Kenan Berbic <kberbic@Kenans-MacBook-Pro.local>
2021-01-20 01:43:51 +0100

    Init

--- main.tf
+++ main.tf
@@ -7,12 +7,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region                  = "us-west-2"
-  shared_credentials_file = "/.aws/credentials"
-  profile                 = "default"
-}
-
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
